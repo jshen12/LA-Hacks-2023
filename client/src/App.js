7ca9287611958a18ebc43774d397cf45c45435d0
jae8833 <jaelee8210@gmail.com>
2023-04-22 16:59:18 -0700

    complete listing component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import Listings from "./components/Listings";
 import CreateListing from "./pages/CreateListing";
 import React, { useState } from "react";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   const [isPopUpForm, setIsPopUpForm] = useState(false);
 
   return (
     <div className="App">
-      
+      <SignUpPage />
     </div>
   );
 }
