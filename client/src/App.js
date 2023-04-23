5d9eba1e87242e590bd4a8814f00cf1a56b4314f
cherylwu834 <124432604+cherylwu834@users.noreply.github.com>
2023-04-22 19:40:06 -0700

    Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './Layout.js';
 
 function App() {
   return (
     <div className="App">
+      <Layout />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <img src={logo} className="App-logo" alt="logo" />
+          Hi, Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,7 +21,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    
+
+  </div>
   );
 }
 
